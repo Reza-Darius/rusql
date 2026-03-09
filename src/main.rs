22447d0fc1060e76a50f644ac969a748b44219d2
Darius <Reza.Blersch@gmail.com>
2026-03-09 17:28:48 +0100

    added license

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,9 @@
 use std::error::Error;
 
-use rusql::{Database, Query};
-
-struct User {
-    name: String,
-    age: u8,
-}
+use rusql::Database;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let db = Database::open("my_database");
-
-    let query = Query::new(
-        r#"
-            CREATE TABLE my_table (
-                id = INT,
-                name = STR,
-                age = INT,
-            );
-        "#,
-    );
-
-    db.execute(query)?;
-
-    let users = vec![
-        User {
-            name: "Alice".to_string(),
-            age: 20,
-        },
-        User {
-            name: "Bob".to_string(),
-            age: 25,
-        },
-        User {
-            name: "Charlie".to_string(),
-            age: 30,
-        },
-    ];
-
-    // insert some data!
-    for (idx, user) in users.iter().enumerate() {
-        let query = Query::new("INSERT INTO my_table (id, name, age) VALUES ?, ?, ?;")
-            .bind(idx as i64)
-            .bind(user.name.as_str())
-            .bind(user.age);
-
-        db.execute(query)?;
-    }
-
-    // we can execute queries directly
-    let response = db.execute("SELECT * FROM my_table;".into())?;
-    let rows = response.get_rows().unwrap();
-
-    assert_eq!(rows[0][0], 0);
-    assert_eq!(rows[0][1], "Alice");
-    assert_eq!(rows[0][2], 20);
-
-    assert_eq!(rows[1][0], 1);
-    assert_eq!(rows[1][1], "Bob");
-    assert_eq!(rows[1][2], 25);
-
-    assert_eq!(rows[2][0], 2);
-    assert_eq!(rows[2][1], "Charlie");
-    assert_eq!(rows[2][2], 30);
-
+    repl()?;
     Ok(())
 }
 
